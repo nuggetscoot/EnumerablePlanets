fd32ac1b8602308d9325131bd90afb0698deb25a
Karly Berrning <berningkarly@Karlys-MacBook-Air.local>
2024-01-25 18:47:19 -0600

    enums exercises

---
+++ src/main/java/org/launchcode/EnumerablePlanets/enumerablePlanets/EnumerablePlanetsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.EnumerablePlanets.enumerablePlanets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumerablePlanetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnumerablePlanetsApplication.class, args);
+	}
+
+}
